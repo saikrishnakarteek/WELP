574af8258e304d4d8da99df35c5e73154388baaa
saikrishnakarteek <81621324+saikrishnakarteek@users.noreply.github.com>
2021-11-05 14:54:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+prediction_1=""
+prediction_2=""
+
+Webcam.set({
+ Width:350,
+ height:300,
+ image_format:"png",
+ png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+
+});
+}
+console.log("ml5 version:",ml5.version)
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Q7bSH3w7O/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first prediction is"+prediction_1;
+    speak_data_2="The second prediction is"+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+ 
+}
+
